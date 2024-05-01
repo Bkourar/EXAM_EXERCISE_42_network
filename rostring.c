9dcdb1fce8de21b51b693896c7ed3d7ad5a1c07c
BILAL KOURAR <149763905+Bkourar@users.noreply.github.com>
2024-05-01 11:56:37 +0100

    implementation rostring function in exam 1337 & 42
    Assignment name  : rostring
    Expected files   : rostring.c
    Allowed functions: write, malloc, free
    --------------------------------------------------------------------------------
    
    Write a program that takes a string and displays this string after rotating it
    one word to the left.
    
    Thus, the first word becomes the last, and others stay in the same order.
    
    A "word" is defined as a part of a string delimited either by spaces/tabs, or
    by the start/end of the string.
    
    Words will be separated by only one space in the output.
    
    If there's less than one argument, the program displays \n.
    
    Example:
    
    $>./rostring "abc   " | cat -e
    abc$
    $>
    $>./rostring "Que la      lumiere soit et la lumiere fut"
    la lumiere soit et la lumiere fut Que
    $>
    $>./rostring "     AkjhZ zLKIJz , 23y"
    zLKIJz , 23y AkjhZ
    $>
    $>./rostring "first" "2" "11000000"
    first
    $>
    $>./rostring | cat -e
    $
    $>

---
+++ rostring.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+    while (*str)
+        write(1, str++, 1);
+}
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+    char    stack[1024];
+    
+    if (ac < 2)
+        return (write(1, "\n", 1), 1);
+    i = 0;
+    j = 0;
+    while (av[1][i] && av[1][i] == ' ' || av[1][i] == '\t')
+        i++;
+    while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
+        stack[j++] = av[1][i++];
+    stack[i] = '\0';
+    while (av[1][i] && av[1][i] == ' ' || av[1][i] == '\t')
+        i++;
+    if (!av[1][i])
+        return (ft_putstr(stack), write(1, "\n", 1), 0);
+    while (av[1][i])
+    {
+        if (av[1][i] != ' ' && av[1][i] != '\t')
+            write(1, &av[1][i], 1);
+        else if (av[1][i + 1] == '\0' && (av[1][i] == ' ' || av[1][i] == '\t'))
+            break ;
+        else if (((av[1][i] == ' ' || av[1][i] == '\t') && av[1][i + 1] != ' ' && av[1][i + 1] != '\t'))
+            write(1, &av[1][i], 1);
+        i++;
+    }
+    write(1, " ", 1), ft_putstr(stack), write(1, "\n", 1);
+}
